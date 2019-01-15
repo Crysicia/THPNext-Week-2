85e93222e78e06bb8472f5e79244788be0effd49
Crysicia <lucienmontmartre@yahoo.fr>
2019-01-15 16:02:57 +0100

    Devise routes & Prevent user to login as an other type of user

---
+++ app/controllers/concerns/accessible.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Accessible
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :check_user
+  end
+
+  protected
+
+  def check_user
+    if current_admin
+      flash.clear
+      redirect_to(administration_path) && return
+    elsif current_user
+      flash.clear
+      redirect_to(root_path) && return
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,20 @@
 
 Rails.application.routes.draw do
   root 'home#landing_page'
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations',
+    confirmations: 'admins/confirmations'
+  }
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
+  }
+
   namespace 'administration' do
     get '/', to: 'items#index'
     resources :items
