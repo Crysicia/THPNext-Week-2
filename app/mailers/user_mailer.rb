94682684bb169ae098613a6a447e9cda8db30d15
Crysicia <lucienmontmartre@yahoo.fr>
2019-01-16 16:51:22 +0100

    Add #offer mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
+  default from: 'recommendation@next.alpha'
+  def offer(user, text)
+    @user = user
+    @text = text
+    mail(to: @user.email, subject: 'Check out our new offer!')
+  end
 end
