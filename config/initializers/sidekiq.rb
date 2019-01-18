3092c7ba82e2baca3dcd93d803f94143da1c1873
Crysicia <lucienmontmartre@yahoo.fr>
2019-01-18 13:37:22 +0100

    Heroku experimentations

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], size: 1 }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
