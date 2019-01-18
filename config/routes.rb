2a7bf237d8516de2e57250e31e483b499c644d05
Crysicia <lucienmontmartre@yahoo.fr>
2019-01-18 12:46:54 +0100

    Add Sidekiq monitoring panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  require "sidekiq/web"
+  if Rails.env.production?
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+    end
+  end
+  mount Sidekiq::Web, at: "/sidekiq"
+
   root 'home#landing_page'
   devise_for :admins, controllers: {
     sessions: 'admins/sessions',
